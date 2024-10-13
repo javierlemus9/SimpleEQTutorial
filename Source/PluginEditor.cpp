a91ad4ff4ce47a9779b272539205ca189a44e434
Javier Lemus <javierlemus@Mac.attlocal.net>
2024-10-13 13:28:08 -0400

    added gain labels to grid

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -340,7 +340,6 @@ void ResponseCurveComponent::resized()
         g.drawVerticalLine(x, top, bottom);
     }
     
-    
     Array<float> gain
     {
         -24, -12, 0, 12, 24
@@ -384,6 +383,27 @@ void ResponseCurveComponent::resized()
         r.setSize(textWidth, fontHeight);
         r.setCentre(x, 0);
         r.setY(1);
+        
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+    }
+    
+    for(auto gDb : gain)
+    {
+        auto y  = jmap(gDb, -24.f, 24.f, float(bottom), float(top));
+        
+        String str;
+        if(gDb > 0)
+            str << "+";
+        str << gDb;
+        
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+        
+        Rectangle<int> r;
+        r.setSize(textWidth, fontHeight);
+        r.setX(getWidth() - textWidth);
+        r.setCentre(r.getCentreX(), y);
+        
+        g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::lightgrey);
         
         g.drawFittedText(str, r, juce::Justification::centred, 1);
     }
